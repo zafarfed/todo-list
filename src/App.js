65d22620e0d418e41478ea3c920edff78b042eaa
zafarfed <zafarstyle@mail.ru>
2022-03-24 23:40:21 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,95 +1,87 @@
 // import logo from './logo.svg';
 import './App.css';
-// import {useState} from 'react'
-import Todohead from './Todolist/Todohead';
-import Todobody from './Todolist/Todobody';
-import Todofoot from './Todolist/Todofoot';
+import {useState, useRef} from 'react'
+import AOS  from 'aos';
+import 'aos/dist/aos.css'
 
 
 function App() {
+
+const ref = useRef()
+const date = new Date()
+
+AOS.init({duration:5000})
+
+    const [inputValue, setInputValue] = useState('')
+    const [mytodo, setMytodo] = useState(JSON.parse(localStorage.getItem('mytodo')) || [] )  
+
+function deleteOneItem(itemId){
+const items = mytodo.filter((value)=>value.id !== itemId)
+setMytodo(items)
+localStorage.setItem('mytodo', JSON.stringify(items))  
+
+}
+function selected(itemId){
+const items = mytodo.filter((value)=>value.id !== itemId)
+ref.current.style.background = 'yellowgreen'  
+
+}
+
+
+    function add(){
+     
+      const newItem = [...mytodo,{
+               id:mytodo.length+1,
+               text:inputValue
+           }]
+           
+           setMytodo(newItem)
+           setInputValue('')
+           localStorage.setItem('mytodo', JSON.stringify(newItem))  
+   }
+
+   
+  function clear(){
+    localStorage.clear('mytodo')
+} 
+
+
 return (
   <div className='my_todo'>
-    <Todohead/>
-    <hr/>
-    <Todobody/>
-    <hr/>
-    <Todofoot/>
+    
+    <div className="todo_head">
+        <h1>My Todo list</h1>
+        <a> {date.getDate()}. {date.getMonth()+1}. {date.getFullYear()}  {date.getHours()}:{date.getMinutes()}</a>
+        <button onClick={clear} style={{padding:'3px', background:'red', color:'wheat',cursor:'pointer',float:'right',marginRight:'5px'}}
+        >Clear all</button>
+    </div>
+
+<hr/>
+
+    <div className="todo_body">
+        <div>
+        {mytodo.map((item, index)=> <div className='mappedItem ' data-aos={'fade-up'} key={index} ref={ref}>
+            <p>{index+1}</p>. <p className='secondP'>{item.text}</p> 
+            <p> {date.getDate()}. {date.getMonth()+1}. {date.getFullYear()}  {date.getHours()}:{date.getMinutes()}</p>
+            <button onClick={()=>selected(item.id)}>⭐</button>  
+            <button onClick={()=>deleteOneItem(item.id)}>❌</button>
+        </div>  
+        
+        )}
+        </div>
+        </div>
+
+        <hr/>
+
+        <div className="todo_foot">
+            <input type={'text'} placeholder="Text here ..." onChange={(e)=>setInputValue(e.target.value)}value={inputValue}/>
+            <button className="add_btn" title="Add to todo list" onClick={add}>✔</button>
+        
+        </div>
   </div>
 )
 
-// const [user, setUser] = useState('')
-// const [data, setData] = useState([
-//   {id:1, name:'Zafar'},
-//   {id:2, name:'Nodir'},
-//   {id:3, name:'Sadaddin'},
-//   {id:4, name:'Sulaymon'},
 
-// ])
-// const [editedName, setEditedName] = useState(null)
-// const [changedName, setChangedName] = useState(null)
-
-// function deleteUser(itemId){
-// const users = data.filter((value)=>value.id !== itemId)
-// setData(users)
-// }
-
-// function addUser(e){
-//   e.preventDefault()
-// const newUser = [...data, { 
-//   id:data.length+1,
-//   name:user
-// }]
-// setData(newUser)
-// }
-
-// function editUser(item){
-//   setEditedName(item.id)
-//   setChangedName(item.name)
-// }
-
-// function save(){
-//   const saveArray = data.map((item)=>editedName ==item.id?{...item, name:changedName}:item)
-//   setData(saveArray)
-//   setEditedName(null)
-// }
-
-//   return (
-//     <div className="App">
-//       <h1>Todo list</h1>
-//       <form onSubmit={addUser}>
-//           <input type={'text'} className='p-1' onChange={(e)=>setUser(e.target.value)}/>
-//           <button type='submit' className='btn btn-success'>Add</button>
-//       </form>
-//       <table className='table'>
-//         <thead>
-//         <tr>
-//           <th>ID</th>
-//           <th>User</th>
-//           <th>Actions</th>
-//         </tr>
-//         </thead>
-//         <tbody>
-//             {
-//             data.map((item,index)=>{return <tr key={index}>
-            
-//               <td>{index+1}</td>
-//               <td style={{width:'200px'}}>{editedName==item.id? <input type={'text'}
-//                onChange={(e)=>setChangedName(e.target.value)} value={changedName}/> : item.name}</td>
-//               <td>
-//                 {
-//                   editedName==item.id ? 
-//                   <button onClick={save}>✔</button> :
-//                   <button onClick={()=>editUser(item)}> ✏</button>
-//                 }
-//                 <button onClick={()=>deleteUser(item.id)}>❌</button>
-//                 </td>
-
-//               </tr>}
-//             )}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
 }
 
 export default App;
